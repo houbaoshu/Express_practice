7012ce9a4c3b5b0bc42b47b891ee8f5c493370be
houbaoshu <jshoubaoshu@outlook.com>
2021-06-12 19:36:15 +0800

    已完成admin部分

--- 快递e栈/src/com/kaikeba/dao/BaseAdminDao.java
+++ 快递e栈/src/com/kaikeba/dao/BaseAdminDao.java
@@ -1,6 +1,9 @@
 package com.kaikeba.dao;
 
+import com.kaikeba.bean.Admin;
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * 用于定义admin表格的操作规范
@@ -13,7 +16,7 @@ public interface BaseAdminDao {
      * @param date
      * @param ip
      */
-    void updateLoginTime (String username, Date date, String ip);
+    void updateLoginTimeAndIP (String username, Date date, String ip);
 
     /**
      * 管理员根据账号密码登陆
@@ -22,4 +25,44 @@ public interface BaseAdminDao {
      * @return
      */
     boolean login(String username, String password);
+
+
+    /**
+     * 查找所有管理员
+     * @param limit
+     * @param offset
+     * @param pageNumber
+     * @return
+     */
+    List<Admin> findAll(boolean limit, int offset, int pageNumber);
+
+    /**
+     * 根据用户手机号码查找管理员信息
+     * @param userPhone
+     * @return
+     */
+    Admin findByUserPhone(String userPhone);
+
+    /**
+     * 添加管理员,需要username,password,userPhone,IDCardNumber
+     * @param admin
+     * @return
+     */
+    boolean insert(Admin admin);
+
+    /**
+     * 根据id删除管理员
+     * @param id
+     * @return
+     */
+    boolean delete(int id);
+
+    /**
+     * 更新管理员，可更新项username,password,userPhone,IDCardNumber
+     * @param id
+     * @param admin
+     * @return
+     */
+    boolean update(int id,Admin admin);
+
 }
--- 快递e栈/src/com/kaikeba/service/AdminService.java
+++ 快递e栈/src/com/kaikeba/service/AdminService.java
@@ -1,31 +1,88 @@
 package com.kaikeba.service;
 
+import com.kaikeba.bean.Admin;
 import com.kaikeba.dao.BaseAdminDao;
 import com.kaikeba.dao.impl.AdminDaoMysql;
 
 import java.util.Date;
+import java.util.List;
 
 public class AdminService {
     private static BaseAdminDao dao = new AdminDaoMysql();
 
     /**
      * 更新登录时间和ip
+     *
      * @param username
      * @param date
      * @param ip
      */
     public static void updateLoginTimeAndIP(String username, Date date, String ip) {
-        dao.updateLoginTime(username, date,ip);
+        dao.updateLoginTimeAndIP(username, date, ip);
     }
 
     /**
      * 登录
+     *
      * @param username
      * @param password
      * @return
      */
     public static boolean login(String username, String password) {
-        return dao.login(username,password);
+        return dao.login(username, password);
+    }
+
+    /**
+     * 查找所有管理员
+     *
+     * @param limit
+     * @param offset
+     * @param pageNumber
+     * @return
+     */
+    public static List<Admin> findAll(boolean limit, int offset, int pageNumber) {
+        return dao.findAll(limit, offset, pageNumber);
+    }
+
+    /**
+     * 根据用户手机号码查找管理员信息
+     *
+     * @param userPhone
+     * @return
+     */
+    public static Admin findByUserPhone(String userPhone) {
+        return dao.findByUserPhone(userPhone);
+    }
+
+    /**
+     * 添加管理员,需要username,password,userPhone,IDCardNumber
+     *
+     * @param admin
+     * @return
+     */
+    public static boolean insert(Admin admin) {
+        return dao.insert(admin);
+    }
+
+    /**
+     * 根据id删除管理员
+     *
+     * @param id
+     * @return
+     */
+    public static boolean delete(int id) {
+        return dao.delete(id);
+    }
+
+    /**
+     * 更新管理员，可更新项username,password,userPhone,IDCardNumber
+     *
+     * @param id
+     * @param admin
+     * @return
+     */
+    public static boolean update(int id, Admin admin) {
+        return dao.update(id,admin);
     }
 
 }
