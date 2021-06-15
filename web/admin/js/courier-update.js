bb58a5ef384c5d5d8c207afdfb570c5619a7ab7d
houbaoshu <jshoubaoshu@outlook.com>
2021-06-15 15:37:12 +0800

    layer关闭出错

--- web/admin/js/courier-update.js
+++ web/admin/js/courier-update.js
@@ -6,12 +6,12 @@ $(document).ready(() => {
     let g_userPhone = null;
     // 查询成功后显示快递员信息
     $(".btn-info:eq(0)").on('click',() => {
-        // let windowId = layer.load;
+        let windowId = layer.load;
         let userPhone = $(".inline-input").val();
         $.getJSON('/admin/find.do',{'userPhone':userPhone},function (data) {
-            // layer.close(windowId);
+            layer.close(windowId);
             layer.msg(data.result);
-            if (data.status == 0) {
+            if (data.status === 0) {
                 $(".header:eq(1),.content:eq(1)").fadeIn(1000);
                 adminId = data.data.id;
                 g_userPhone = data.data.userPhone;
@@ -24,7 +24,7 @@ $(document).ready(() => {
     });
     //删除快递员
     $(".btn-info:eq(1)").on('click',() => {
-        // let windowId = layer.load();
+        let windowId = layer.load();
         let username = $("input:eq(1)").val();
         let userPhone = $("input:eq(2)").val();
         let idCardNumber = $("input:eq(3)").val();
