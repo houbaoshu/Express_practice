d4531c6745409aa282b599e0f45e1f5ccd6b276d
houbaoshu <jshoubaoshu@outlook.com>
2021-06-24 22:01:22 +0800

    完成wechat ExpressControl 部分

--- src/com/kaikeba/util/UserUtil.java
+++ src/com/kaikeba/util/UserUtil.java
@@ -1,13 +1,48 @@
 package com.kaikeba.util;
 
+import com.kaikeba.bean.Admin;
+import com.kaikeba.bean.User;
+
 import javax.servlet.http.HttpSession;
 
 public class UserUtil {
+    /**
+     *
+     * @param session
+     * @return 录入人手机号码
+     */
     public static String getUserPhone(HttpSession session) {
         return "18888888888";
     }
-
+    /**
+     *
+     * @param session
+     * @return 录入人姓名
+     */
     public static String getUserName(HttpSession session) {
-        return (String)session.getAttribute("AdminUsername");
+        return (String) session.getAttribute("userName");
+    }
+    //微信的对象哪生成的？
+    public static Object getWxObject(HttpSession session) {
+        return session.getAttribute("WxObject");
+    }
+    //字符识别身份？
+    public static String getIdentity(HttpSession session) {
+        //session中获得身份信息
+        return (String) session.getAttribute("identity");
+    }
+    public static String getWxPhone(HttpSession session)  {
+        String identity = getIdentity(session);
+        String userPhone = null;
+        //是管理员则获得管理员用户对象,再获得手机号码
+        if ("admin".equals(identity)) {
+            Admin admin = (Admin) session.getAttribute("WxObject");
+            userPhone = admin.getUserPhone();
+        } else if ("user".equals(identity)) {
+            //是普通用户则获得普通用户对象
+            User user = (User) session.getAttribute("WxObject");
+            userPhone = user.getUserPhone();
+        }
+        return userPhone;
     }
 }
